4f24f664853d287c0d14902890ea036948cda7fd
anurag chhaperwal <anuragchamp71@gmail.com>
2021-07-23 03:41:52 +0530

    test program

--- src/Recursion/Test01.java
+++ src/Recursion/Test01.java
@@ -1,4 +1,15 @@
 package Recursion;
 
 public class Test01 {
+
+    public static void main(String[] args) {
+        test(5);
+    }
+    public static void test(int i){
+        if(i == 0){
+            return ;
+        }
+        System.out.println(i);
+        test(--i);
+    }
 }
