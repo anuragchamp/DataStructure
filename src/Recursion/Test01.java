ce752507e40bca0616eed724c692d9a57e1239a4
anurag chhaperwal <anuragchamp71@gmail.com>
2021-07-23 03:45:05 +0530

    "recursion test program"

--- src/Recursion/Test01.java
+++ src/Recursion/Test01.java
@@ -4,6 +4,7 @@ public class Test01 {
 
     public static void main(String[] args) {
         test(5);
+        System.out.println("hello world");
     }
     public static void test(int i){
         if(i == 0){
