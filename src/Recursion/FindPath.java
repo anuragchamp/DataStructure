91ae4a0f10ff389eb4bdc4134e4d21f7664cf787
anurag chhaperwal <anuragchamp71@gmail.com>
2021-07-23 04:41:58 +0530

    "find path Question"

--- src/Recursion/FindPath.java
+++ src/Recursion/FindPath.java
@@ -1,4 +1,43 @@
 package Recursion;
 
 public class FindPath {
+    public static void main(String[] args) {
+        int arr[][] = {
+                {0,1,0},
+                {0,0,1},
+                {0,0,0}
+        };
+        int ans[][] = new int[3][3];
+        boolean visited[][] = new boolean[3][3];
+
+        printAllPath(arr , ans , 0 ,0 , 2 ,2  , visited);
+    }
+
+    private static void printAllPath(int[][] arr, int[][] ans, int si, int sj, int di, int dj, boolean[][] visited) {
+        if(si < 0 || sj < 0  || si > di || sj > dj || arr[si][sj] == 1 || visited[si][sj]){
+            return;
+        }
+        if(si == di && sj == dj){
+            System.out.println("----------------------");
+            for (int i=0;i<ans.length;i++){
+                for (int j=0;j<ans[0].length ; j++){
+                    System.out.print(ans[i]);
+                }
+                System.out.println();
+            }
+
+            ans[si][sj] = 2;
+            visited[si][sj] = true;
+            printAllPath(arr , ans  , si - 1 , sj , di , dj , visited);
+            printAllPath(arr , ans  , si  , sj - 1 , di , dj , visited);
+            printAllPath(arr , ans  , si + 1 , sj , di , dj , visited);
+            printAllPath(arr , ans  , si , sj + 1 , di , dj , visited);
+
+            ans[si][sj] = 0;
+            visited[si][sj] = false;
+
+            System.out.println("----------------------");
+        }
+
+    }
 }
