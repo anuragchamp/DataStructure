72b4fef4268fc4e664a732a96c21d62e0070aceb
anurag chhaperwal <anuragchamp71@gmail.com>
2021-07-23 05:37:12 +0530

    "find path Question modifications"

--- src/Recursion/FindPath.java
+++ src/Recursion/FindPath.java
@@ -4,29 +4,27 @@ public class FindPath {
     public static void main(String[] args) {
         int arr[][] = {
                 {0,1,0},
-                {0,0,1},
+                {0,0,0},
                 {0,0,0}
         };
         int ans[][] = new int[3][3];
-        boolean visited[][] = new boolean[3][3];
+      //  boolean visited[][] = new boolean[3][3];
 
-        printAllPath(arr , ans , 0 ,0 , 2 ,2  , visited);
-        System.out.println("gg");
+        printAllPath(arr , ans , 0 ,0 , 2 ,2 );
+        //System.out.println("gg");
     }
 
-    private static void printAllPath(int[][] arr, int[][] ans, int si, int sj, int di, int dj, boolean[][] visited) {
-
-
-        System.out.println(si +" " +di);
-        if(si < 0 || sj < 0  || si > di + 1 || sj > dj + 1 || arr[si][sj] == 1 || visited[si][sj]){
+    private static void printAllPath(int[][] arr, int[][] ans, int si, int sj, int di, int dj) {
+        if(si < 0 || sj < 0  || si >= di + 1 || sj >= dj + 1 || arr[si][sj] == 1 || ans[si][sj] == 2 ){
             return;
         }
 
         if(si == di && sj == dj){
+            ans[si][sj] = 2;
             System.out.println("----------------------");
             for (int i=0;i<ans.length;i++) {
                 for (int j = 0; j < ans[0].length; j++) {
-                    System.out.print(ans[i]);
+                    System.out.print(ans[i][j]);
                 }
                 System.out.println();
             }
@@ -34,14 +32,14 @@ public class FindPath {
         }
 
         ans[si][sj] = 2;
-        visited[si][sj] = true;
-        printAllPath(arr , ans  , si - 1 , sj , di , dj , visited);
-        printAllPath(arr , ans  , si  , sj - 1 , di , dj , visited);
-        printAllPath(arr , ans  , si + 1 , sj , di , dj , visited);
-        printAllPath(arr , ans  , si , sj + 1 , di , dj , visited);
+        //visited[si][sj] = true;
+        printAllPath(arr , ans  , si - 1 , sj , di , dj );
+        printAllPath(arr , ans  , si  , sj - 1 , di , dj );
+        printAllPath(arr , ans  , si + 1 , sj , di , dj );
+        printAllPath(arr , ans  , si , sj + 1 , di , dj );
 
         ans[si][sj] = 0;
-        visited[si][sj] = false;
+        //visited[si][sj] = false;
 
     }
 }
