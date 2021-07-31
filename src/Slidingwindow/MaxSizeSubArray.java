f149d3d40eaf1bcbf2ba80ca8f96171aa1d0277a
anurag chhaperwal <anuragchamp71@gmail.com>
2021-07-31 12:02:33 +0530

    "max sum of subarray of size k"

--- src/Slidingwindow/MaxSizeSubArray.java
+++ src/Slidingwindow/MaxSizeSubArray.java
@@ -3,8 +3,22 @@ package Slidingwindow;
 public class MaxSizeSubArray {
     public static void main(String[] args) {
 
+
+
         int arr[] = {2,3,5,2,9,7,1};
+        int size = 3;
+
+
+        //brute force approch
+        maxSubArray01(arr , size);
+
+    }
+
+
+    //brute force approch
+    public  static void maxSubArraySum(int arr[]){
         int max = 0;
+
         for(int i=0;i<arr.length;i++){
             int sum = 0;
             for(int j = i; j < i+3  &&  j < arr.length; j++){
@@ -17,6 +31,32 @@ public class MaxSizeSubArray {
         }
 
         System.out.println(max);
+    }
+
+    //another way of doing this in linear time complexity
+    public static void maxSubArray01(int arr[] , int size){
+
+        int str = 0;
+        int end = 0;
+        int sum = 0;
+        int max = 0;
+        while(end < arr.length){
+            sum += arr[end];
+            if(end - str + 1 < size){
+                end++;
+            }
+            else if(end - str + 1 == size){
+                if(max < sum){
+                    max = sum;
+                }
+                sum -= arr[str];
+                str++;
+                end++;
+            }
+        }
+        System.out.println(max);
 
     }
+
+
 }
